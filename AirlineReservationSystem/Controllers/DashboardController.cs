aa7c051bfe22b19075b08c7aa4e93bfff68f4a58
MrTalha1 <mr.talha0304@gmail.com>
2024-08-25 23:27:52 +0500

    USER DASHBOARD WORK

---
+++ AirlineReservationSystem/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineReservationSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        public IActionResult MyBooking()
+        {
+            return View();
+        }
+        public IActionResult MyProfile()
+        {
+            return View();
+        }public IActionResult Setting()
+        {
+            return View();
+        }
+    }
+}
